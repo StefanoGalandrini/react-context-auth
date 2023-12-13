ed1255ca0349a8f1eb9a692c0a7556e826cb7b37
Stefano <stefe68@gmail.com>
2023-12-13 15:50:33 +0100

    Fix Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,10 @@ import Navbar from "../components/Navbar.jsx";
 import {useAuth} from "../contexts/AuthContext.jsx";
 
 function Dashboard() {
-	const user = useAuth();
+	const auth = useAuth();
+	const user = auth.user;
+
+	console.log(user);
 
 	return (
 		<div className="flex flex-col justify-center items-center h-[85vh] text-white">
@@ -12,9 +15,7 @@ function Dashboard() {
 				alt=""
 			/>
 			<h1 className="mt-20 uppercase text-2xl">Dashboard</h1>
-			<p className="mt-5 text-2xl text-green-500">
-				Benvenuto, {user.name} {user.lastName}
-			</p>
+			<p className="mt-5 text-2xl text-green-500">Benvenuto, {user.name}</p>
 
 			<p className="mt-10 text-stone-400 underline">
 				Clicca su "Blog" e guarda la lista degli articoli per sfogarti un po'
